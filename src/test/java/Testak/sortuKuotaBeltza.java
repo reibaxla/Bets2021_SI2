6a11cb6a4074089bf9ea5418d03608fb6539c42a
Xabier Larrea <reibaxla@gmail.com>
2020-10-17 19:24:53 +0200

    Kutxa txuri eta Beltzaren sorrera. Eta Junit 4 import.

---
+++ src/test/java/Testak/sortuKuotaBeltza.java
@@ -0,0 +1,14 @@
+package Testak;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sortuKuotaBeltza {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/Testak/sortuKuotaTxuria.java
@@ -0,0 +1,15 @@
+package Testak;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sortuKuotaTxuria {
+
+	@Test
+	public void test() {
+
+		
+	}
+
+}
